32c71dcbabc0f4f0733297a68779f4313abb5cf6
Prabhat9129 <117995361+Prabhat9129@users.noreply.github.com>
2023-07-24 13:50:18 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score=null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score);
+// console.log(valueInNumber); 
+// console.log(typeof valueInNumber); 
+
+/*
+"66"=> 66
+"66abc"=> NaN
+"prabhat"=> NaN
+undefined=> NaN
+null=> 0;
+true=> 1; false=> 0;
+*/
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/*
+1=> true;  0=> false;
+""=> false; 
+"prabhat"=> true;
+null => false;
+undefined=> false;
+*/
+
+let someNumber=null
+// console.log(typeof someNumber);
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+/*
+25=> '25'
+true=> 'true'
+undefined=> 'undefined'
+null=> 'null'
+*/
