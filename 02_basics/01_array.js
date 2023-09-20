de5926e6d77f3b6bd20644cf681d2ebc07bb0c6b
Prabhat9129 <117995361+Prabhat9129@users.noreply.github.com>
2023-09-20 12:41:52 +0000

    basic2 array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,11 @@
+console.log('ARRAY')                    // ARRAY
+
+const myArray=[0,1,2,3,4,5]
+console.log(myArray);                  // [ 0, 1, 2, 3, 4, 5 ]
+
+const myArray1=new Array(1,2,3,4,5,6)
+console.log(myArray1);                 // [ 1, 2, 3, 4, 5, 6 ]
+console.log(myArray1[0]);              // 1
+
+myArray1.push(7)
+console.log(myArray1);                // [ 1, 2, 3, 4, 5, 6, 7 ]
